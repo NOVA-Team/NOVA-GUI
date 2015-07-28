8a762f692284c913983e6b274aa53bf165b26b58
Calclavia <admin@calclavia.com>
2015-07-27 23:57:31 -0400

    Migrate NOVA-GUI into a separate plugin

---
+++ src/main/java/nova/core/gui/internal/NovaGui.java
@@ -0,0 +1,13 @@
+package nova.core.gui.internal;
+
+import nova.core.gui.dep.GuiModule;
+import nova.core.loader.NovaMod;
+
+/**
+ * The main class to initialize the Gui Plugin
+ *
+ * @author Calclavia
+ */
+@NovaMod(id = "nova-gui", name = "NOVA-GUI", version = "0.0.1", modules = { GuiModule.class }, novaVersion = "0.1.0")
+public class NovaGui {
+}
