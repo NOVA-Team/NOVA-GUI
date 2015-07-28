702b8c61130e023661c17491d87e6aaa933b3811
Calclavia <admin@calclavia.com>
2015-07-28 12:25:37 -0400

    Fixed errors caused by removing UI

--- mc17/src/main/java/nova/gui/wrapper/mc17/launch/NovaGui.java
+++ mc17/src/main/java/nova/gui/wrapper/mc17/launch/NovaGui.java
@@ -1,7 +1,13 @@
 package nova.gui.wrapper.mc17.launch;
 
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import nova.core.loader.Loadable;
 import nova.core.loader.NovaMod;
+import nova.gui.wrapper.mc17.MCGuiFactory;
 import nova.gui.wrapper.mc17.dependency.GuiModule;
+import nova.wrapper.mc1710.launcher.NovaMinecraft;
 
 /**
  * The main class to initialize the Gui Plugin
@@ -9,5 +15,10 @@ import nova.gui.wrapper.mc17.dependency.GuiModule;
  * @author Calclavia
  */
 @NovaMod(id = "nova-gui", name = "NOVA-GUI", version = "0.0.1", modules = { GuiModule.class }, novaVersion = "0.1.0")
-public class NovaGui {
+public class NovaGui implements Loadable {
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void preInit() {
+		NetworkRegistry.INSTANCE.registerGuiHandler(NovaMinecraft.instance, new MCGuiFactory.GuiHandler());
+	}
 }
